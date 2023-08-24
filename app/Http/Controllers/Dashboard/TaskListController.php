18e5630115d9c875db5ba9775b423651f7ae7b67
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-08-24 19:43:00 +0200

    🛠️ Created resources for TaskController and TaskListController for automated routes and CRUD

---
+++ app/Http/Controllers/Dashboard/TaskListController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TaskList;
+use App\Models\User;
+
+class TaskListController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the task lists.
+     */
+    public function index() {
+        $user = auth()->user();
+        $taskLists = $user->taskLists;
+
+        return response()->json(['taskLists' => $taskLists]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created taskList in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'list_name' => 'required|string',
+                'list_description' => 'nullable|string|max:500',
+            ]);
+
+            $user = $request->user();
+            $taskList = new TaskList([
+                'list_name' => $validatedData['list_name'],
+                'list_description' => $validatedData['list_description'],
+            ]);
+
+            $user->taskLists()->save($taskList);
+
+            return response()->json(['message' => 'La liste de tâches a été créée avec succès !']);
+        } catch (\Exception $e) {
+            $statusCode = $e->getCode() ?: 500;
+            return response()->json(['message' => 'Erreur lors de la création de la liste de tâches'], $statusCode);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,13 +22,18 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::resource('task', App\Http\Controllers\Dashboard\TaskController::class);
+    Route::resource('task-list', App\Http\Controllers\Dashboard\TaskListController::class);
+});
+
 // Routes for task lists
-Route::get('/task-lists', [App\Http\Controllers\TaskListController::class, 'index'])->name('task-lists.index');
-Route::post('/task-lists', [App\Http\Controllers\TaskListController::class, 'store'])->name('task-lists.store');
+// Route::get('/task-lists', [App\Http\Controllers\TaskListController::class, 'index'])->name('task-lists.index');
+// Route::post('/task-lists', [App\Http\Controllers\TaskListController::class, 'store'])->name('task-lists.store');
 
 
 // Routes for tasks
-Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
-Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
+// Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+// Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
 // Route::put('/tasks/{task}', [App\Http\Controllers\TaskController::class, 'update'])->name('tasks.update');
 // Route::delete('/tasks/{task}', [App\Http\Controllers\TaskController::class, 'destroy'])->name('tasks.destroy');
