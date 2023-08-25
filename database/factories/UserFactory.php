7e8113020c868f9ed63b3af388387e257d2ea2e1
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-08-25 14:30:09 +0200

    :closed_lock_with_key: Update 'password' field in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => bcrypt('password123'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'display_name' => fake()->userName(),
             'profile_image' => 'profile-images/avatar-default.jpg',
             'email_verified_at' => now(),
