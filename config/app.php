cfe89485c7eeecab351fe94c7c6287bb0b30f9af
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-08-25 14:37:37 +0200

    :pencil2: Update default app name in config/app.php

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Gestionnaire de tâches collaboratif'),
 
     /*
     |--------------------------------------------------------------------------
