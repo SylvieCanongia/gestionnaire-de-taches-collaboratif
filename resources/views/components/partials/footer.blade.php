d685bd6cce7117d2f742899ffc405bbdbf25393a
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-08-29 18:53:12 +0200

    🧩 Added 'header' and 'footer' Blade components. Updated SCSS breakpoints to match Bootstrap 5 breakpoints.

---
+++ resources/views/components/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    &copy; <script>document.write(new Date().getFullYear())</script> Mon gestionnaire de tâches. Tous droits réservés.
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <task-list-index :user="{{ Auth::user() }}" :task-lists="{{ json_encode($taskLists) }}" task-list-create-url="{{ route('task-list.create') }}"></task-list-index>
-</div>
+<!-- <div class="container"> -->
+    <task-list-index :user="{{ Auth::user() }}" :task-lists="{{ Js::from($taskLists) }}" task-list-create-url="{{ route('task-list.create') }}"></task-list-index>
+<!-- </div> -->
 @endsection
